1e342896cb211a71f487ded9cf1ef3d827138d86
Daniel Biegler <DanielBiegler@users.noreply.github.com>
2019-02-21 12:21:43 +0100

    add toggle for buttons and remove magic variables
    some comments too

--- index.php
+++ index.php
@@ -1,58 +1,101 @@
 <?php
-$path_gpio = '/sys/class/gpio';
-$path_gpio_export = '/sys/class/gpio/export';
+// For dev purposes enable all errors and warnings
+error_reporting(E_ALL);
 
-$path_gpio_cup_one = '/sys/class/gpio/gpio2';
-$path_gpio_cup_two = '/sys/class/gpio/gpio3';
-$path_gpio_power = '/sys/class/gpio/gpio4';
+$path_gpio_power = '/sys/class/gpio/gpio2';
+$path_gpio_cup_one = '/sys/class/gpio/gpio3';
+$path_gpio_cup_two = '/sys/class/gpio/gpio4';
 
+$GPIO_ON = '0';
+$GPIO_OFF = '1';
+
+$BUTTON_PRESS_DELAY = 500000; // in microseconds
+
+$HTTP_RESPONSE_CODE_OK = 200;
+$HTTP_RESPONSE_CODE_SERVERERROR = 500;
+
+/**
+ * Turns the coffee pins on and - after a delay - off again.
+ * @return int Either response code $HTTP_RESPONSE_CODE_OK for success, or response code $HTTP_RESPONSE_CODE_SERVERERROR for failure.
+ */
 function make_coffee($num_cups)
 {
-    global $path_gpio_cup_one, $path_gpio_cup_two;
+    global $path_gpio_cup_one
+    , $path_gpio_cup_two
+    , $GPIO_ON
+    , $GPIO_OFF
+    , $BUTTON_PRESS_DELAY
+    , $HTTP_RESPONSE_CODE_OK
+    , $HTTP_RESPONSE_CODE_SERVERERROR;
     
     if($num_cups === '1')
     {
         $path_gpio_cup_one_value = $path_gpio_cup_one.'/value';
         $currentValue = file_get_contents($path_gpio_cup_one_value);
-        $result = file_put_contents($path_gpio_cup_one_value, $currentValue[0] === "0" ? 1 : 0);
+        $result = file_put_contents($path_gpio_cup_one_value, $GPIO_ON); // turn on
+
+        usleep($BUTTON_PRESS_DELAY);
+
+        $currentValue = file_get_contents($path_gpio_cup_one_value);
+        $result = file_put_contents($path_gpio_cup_one_value, $GPIO_OFF); // turn off
 
         if($result) {
-            return 200;
+            return $HTTP_RESPONSE_CODE_OK;
         } else {
-            return 500;
+            return $HTTP_RESPONSE_CODE_SERVERERROR;
         }
     } 
     elseif($num_cups === '2')
     {
         $path_gpio_cup_two_value = $path_gpio_cup_two.'/value';
         $currentValue = file_get_contents($path_gpio_cup_two_value);
-        $result = file_put_contents($path_gpio_cup_two_value, $currentValue[0] === "0" ? 1 : 0);
+        $result = file_put_contents($path_gpio_cup_two_value, $GPIO_ON);
+
+        usleep($BUTTON_PRESS_DELAY);
+
+        $currentValue = file_get_contents($path_gpio_cup_two_value);
+        $result = file_put_contents($path_gpio_cup_two_value, $GPIO_OFF);
 
         if($result) {
-            return 200;
+            return $HTTP_RESPONSE_CODE_OK;
         } else {
-            return 500;
+            return $HTTP_RESPONSE_CODE_SERVERERROR;
         }
     }
     else
     {
         echo "Error: Invalid cup amount. The only valid options are either '1' or '2'.";
-        return 503;
+        return $HTTP_RESPONSE_CODE_SERVERERROR;
     }
 }
 
-
+/**
+ * Reads the current value of the pin for power and toggles it.
+ * @return int Either response code $HTTP_RESPONSE_CODE_OK for success, or response code $HTTP_RESPONSE_CODE_SERVERERROR for failure.
+ */
 function toggle_power()
 {
-    global $path_gpio_power;
+    global $path_gpio_power
+    , $GPIO_ON
+    , $GPIO_OFF
+    , $BUTTON_PRESS_DELAY
+    , $HTTP_RESPONSE_CODE_OK
+    , $HTTP_RESPONSE_CODE_SERVERERROR;
+
     $path_gpio_power_value = $path_gpio_power.'/value';
 
     $currentValue = file_get_contents($path_gpio_power_value);
-    $result = file_put_contents($path_gpio_power_value, $currentValue[0] === "0" ? 1 : 0);
+    $result = file_put_contents($path_gpio_power_value, $currentValue[0] === $GPIO_ON ? $GPIO_OFF : $GPIO_ON);
+
+    usleep($BUTTON_PRESS_DELAY);
+
+    $currentValue = file_get_contents($path_gpio_power_value);
+    $result = file_put_contents($path_gpio_power_value, $currentValue[0] === $GPIO_ON ? $GPIO_OFF : $GPIO_ON);
+    
     if($result) {
-        return 200;
+        return $HTTP_RESPONSE_CODE_OK;
     } else {
-        return 500;
+        return $HTTP_RESPONSE_CODE_SERVERERROR;
     }
 }
 
@@ -67,7 +110,7 @@ if(isset($_POST['cups']))
     }
     else
     {
-        http_response_code(400);
+        http_response_code($HTTP_RESPONSE_CODE_SERVERERROR);
         echo "Error: Invalid cup amount. The only valid options are either '1' or '2'.";
         die;
     }
@@ -77,7 +120,7 @@ elseif(isset($_POST['power']))
     if($_POST['power'] !== "toggle")
     {
         echo "Error: Invalid power value. The only valid option is 'toggle'.";
-        http_response_code(503);
+        http_response_code($HTTP_RESPONSE_CODE_SERVERERROR);
         die;
     }
     else
@@ -133,11 +176,12 @@ elseif(isset($_POST['power']))
     <script>
 
 
+        // // // ONE CUP
         const btnOneCup = document.getElementById('btn-one-cup');
         btnOneCup.addEventListener('click', e => {
             function handleResponse() {
                 let xhttp = this;
-                if(xhttp.status === 200) {
+                if(xhttp.status === <?php echo $HTTP_RESPONSE_CODE_OK; ?>) {
                     setTimeout(() => {
                         btnOneCup.classList.remove('is-loading');
                         btnOneCup.classList.add('is-success');
@@ -164,11 +208,12 @@ elseif(isset($_POST['power']))
         });
 
 
+        // // // TWO CUPS
         const btnTwoCups = document.getElementById('btn-two-cups');
         btnTwoCups.addEventListener('click', e => {
             function handleResponse() {
                 let xhttp = this;
-                if(xhttp.status === 200) {
+                if(xhttp.status === <?php echo $HTTP_RESPONSE_CODE_OK; ?>) {
                     setTimeout(() => {
                         btnTwoCups.classList.remove('is-loading');
                         btnTwoCups.classList.add('is-success');
@@ -195,11 +240,12 @@ elseif(isset($_POST['power']))
         });
 
 
+        // // // POWER
         const btnPower = document.getElementById('btn-power');
         btnPower.addEventListener('click', e => {
             function handleResponse() {
                 let xhttp = this;
-                if(xhttp.status === 200) {
+                if(xhttp.status === <?php echo $HTTP_RESPONSE_CODE_OK; ?>) {
                     setTimeout(() => {
                         btnPower.classList.remove('is-loading');
                         btnPower.classList.add('is-success');
